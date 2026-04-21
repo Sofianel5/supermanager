885411c296a2754cbae80319173eeaa2a07b88c5
Sofianel5 <sofianef1110@gmail.com>
2026-04-20 22:05:08 -0700

    refactor: generate activity HTTP types from Rust

--- crates/reporter-protocol/src/lib.rs
+++ crates/reporter-protocol/src/lib.rs
@@ -1,8 +1,10 @@
 mod hook;
+mod http;
 mod project;
 mod summary;
 
 pub use hook::{FeedResponse, HookTurnReport, IngestResponse, StoredHookEvent, UploadedTranscript};
+pub use http::{ActivityUpdate, ActivityUpdatesResponse};
 pub use project::{CreateProjectRequest, CreateProjectResponse, Project, ProjectMetadataResponse};
 pub use summary::{
     MemberSnapshot, OrganizationSnapshot, ProjectBlufSnapshot, ProjectSnapshot, SummaryStatus,
